216b83fd09f0434f594ac1b83859ad91490ac007
François <francois.burgener@hotmail.fr>
2018-06-10 15:04:47 +0200

    Etape 2b

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,37 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+    res.send(generateAdress());
+});
+
+app.listen(3000, function () {
+    console.log("Accepting HTTP requests on port 3000");
+});
+
+function generateAdress(){
+	var numberOfAdresses = chance.integer({
+		min : 1,
+		max : 10
+	});
+	
+	console.log(numberOfAdresses);
+	
+	var adresses = [];
+	
+	for(var i = 0; i < numberOfAdresses; ++i){
+		
+		adresses.push({
+			'street'  : chance.street(),
+			'city'  : chance.city(),
+			'postal'  : chance.postal(),
+			'country' : chance.country({full : true})
+		});
+	}
+	console.log(adresses);
+	return adresses;
+}
