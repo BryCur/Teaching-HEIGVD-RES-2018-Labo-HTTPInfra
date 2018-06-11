a033fc86bc3d5a4241063d6e533648591ac2e2ac
François <francois.burgener@hotmail.fr>
2018-06-11 20:32:09 +0200

    Etape 5

---
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -0,0 +1,14 @@
+<?php
+	$DYNAMIC_APP = getenv('DYNAMIC_APP');
+	$STATIC_APP = getenv('STATIC_APP');
+?>
+
+<VirtualHost *:80>
+    ServerName demo.res.ch
+
+    ProxyPass "/api/addresses/" "http://<?php print $DYNAMIC_APP ?>/"
+    ProxyPassReverse "/api/addresses/" "http://<?php print $DYNAMIC_APP ?>/"
+    
+    ProxyPass "/" "http://<?php print $STATIC_APP ?>/"
+    ProxyPassReverse "/" "http://<?php print $STATIC_APP ?>/"
+</VirtualHost>
